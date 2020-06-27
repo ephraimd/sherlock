b0c71220da71a7e627c752deeabb19d36cc959a9
ephraimd <olagokedammy@gmail.com>
2020-06-27 03:58:13 +0100

    Still Under construction

--- src/test/java/ephraim/comms/SlackServiceTest.java
+++ src/test/java/ephraim/comms/SlackServiceTest.java
@@ -7,14 +7,14 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SlackServiceTest {
-    private final String slackWebHookUrl = "https://hooks.slack.com/services/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";
+    private final String slackWebHookUrl = "";
 
     @Test
     void sendErrorMessage() {
         SlackService slackService = new SlackService(slackWebHookUrl);
-        Assertions.assertDoesNotThrow(() -> slackService.sendErrorMessage("Systems Monitor", "Running slack test for error messages"),
+        Assertions.assertDoesNotThrow(() -> slackService.sendErrorMessage("Sherlock", "Running slack test for error messages"),
                 "Slack error message service isn't functioning properly");
-        Assertions.assertDoesNotThrow(() -> slackService.sendMessage("Systems Monitor", "Running slack test for ordinary messages"),
+        Assertions.assertDoesNotThrow(() -> slackService.sendMessage("Sherlock", "Running slack test for ordinary messages"),
                 "Slack service isn't functioning properly");
     }
 }
